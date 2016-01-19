f50f2e638f345581841fc48fa49b8fb72d1b0bea
awecea <awecea@gmail.com>
2016-01-19 12:41:10 +0200

    initialize fragments

--- app/src/main/java/com/slava/chat/MainActivity.java
+++ app/src/main/java/com/slava/chat/MainActivity.java
@@ -13,6 +13,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.slava.chat.fragments.FragmentContacts;
+import com.slava.chat.fragments.FragmentProfile;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -40,6 +43,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentContacts fcontacts = new FragmentContacts();
+        FragmentProfile fprofile = new FragmentProfile();
     }
 
     @Override
