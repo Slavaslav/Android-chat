603397ffc0fd8fd59aef5a0d07b7f2539e2c742b
awecea <awecea@gmail.com>
2016-01-20 23:36:04 +0200

    delete FloatingActionButton

--- app/src/main/java/com/slava/chat/MainActivity.java
+++ app/src/main/java/com/slava/chat/MainActivity.java
@@ -2,9 +2,7 @@ package com.slava.chat;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
@@ -12,7 +10,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,7 +20,6 @@ import com.slava.chat.fragments.FragmentProfile;
 public class MainActivity extends AppCompatActivity
         implements FragmentContacts.OnFragmentInteractionListener, FragmentProfile.OnFragmentInteractionListener, NavigationView.OnNavigationItemSelectedListener {
 
-    private static final String TAG = "myLogs";
     FragmentContacts fcontacts;
     FragmentProfile fprofile;
     ActionBarDrawerToggle toggle;
@@ -35,30 +31,22 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //when press button HomeAsUp
         toggle.setToolbarNavigationClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
         fcontacts = new FragmentContacts();
         fprofile = new FragmentProfile();
