48ca72af8055c05f9e2f65ed52315bedb815b7ac
awecea <awecea@gmail.com>
2016-05-05 23:06:05 +0300

    Get phone number and name.

--- app/src/main/java/com/slava/chat/fragments/FragmentContacts.java
+++ app/src/main/java/com/slava/chat/fragments/FragmentContacts.java
@@ -4,16 +4,12 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,31 +20,13 @@ import com.slava.chat.MainActivity;
 import com.slava.chat.R;
 
 public class FragmentContacts extends Fragment implements
-        LoaderManager.LoaderCallbacks<Cursor>,
         AdapterView.OnItemClickListener {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private final static String[] FROM_COLUMNS = {
-            Build.VERSION.SDK_INT
-                    >= Build.VERSION_CODES.HONEYCOMB ?
-                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
-                    ContactsContract.Contacts.DISPLAY_NAME
-    };
-    private static final int[] TO_IDS = {android.R.id.text1};
-    private static final String[] PROJECTION =
-            {
-                    ContactsContract.Contacts._ID,
-                    ContactsContract.Contacts.LOOKUP_KEY,
-                    Build.VERSION.SDK_INT
-                            >= Build.VERSION_CODES.HONEYCOMB ?
-                            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY :
-                            ContactsContract.Contacts.DISPLAY_NAME
 
-            };
     private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
     private OnFragmentInteractionListener mListener;
-    private SimpleCursorAdapter mCursorAdapter;
     private ListView mContactsList;
 
     public FragmentContacts() {
@@ -81,22 +59,14 @@ public class FragmentContacts extends Fragment implements
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        mCursorAdapter = new SimpleCursorAdapter(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                null,
-                FROM_COLUMNS,
-                TO_IDS,
-                0);
-
-        mContactsList.setAdapter(mCursorAdapter);
+        //mContactsList.setAdapter();
 
         if (ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this.getActivity(),
                     new String[]{Manifest.permission.READ_CONTACTS},
                     MY_PERMISSIONS_REQUEST_READ_CONTACTS);
         } else {
-            getLoaderManager().initLoader(0, null, this);
+            getContactsInfo();
         }
 
         //set Toolbar title
@@ -121,42 +91,13 @@ public class FragmentContacts extends Fragment implements
     }
 
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-
-        return new CursorLoader(
-                getActivity(),
-                ContactsContract.Contacts.CONTENT_URI,
-                PROJECTION,
-                null,
-                null,
-                null
-        );
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        mCursorAdapter.swapCursor(data);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        mCursorAdapter.swapCursor(null);
-    }
-
-    @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                    // tell loader manager to start loading
-                    getLoaderManager().initLoader(0, null, this);
+                    getContactsInfo();
                 }
-                return;
             }
         }
     }
@@ -170,6 +111,20 @@ public class FragmentContacts extends Fragment implements
     public void onResume() {
         super.onResume();
         mListener.setDrawerLockMode(MainActivity.LOCK_MODE_LOCKED_CLOSED);
+    }
+
+    public void getContactsInfo() {
+        Cursor cursor = getActivity().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                Log.d("LOG", "name = " + name);
+                Log.d("LOG", "number = " + phoneNumber);
+
+            }
+            cursor.close();
+        }
     }
 
     public interface OnFragmentInteractionListener {
